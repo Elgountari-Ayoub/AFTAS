3d2efb88dbdd0e567d49a4b71a567007345e29bf
Elgountari-Ayoub <elgountariayoub22@gmail.com>
2023-12-18 12:37:59 +0100

    adding the flowbite dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AFTAS';
+export class AppComponent implements OnInit {
+  title = 'AFTAS';  
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
